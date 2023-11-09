9803854cffad0758cff0f887e78731cfa3307fcc
SESNA-Inteligencia <jcmartinez@sesna.gob.mx>
2023-11-09 15:55:05 -0600

    actualizacion de secciones

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 # Importar librerías
 import dash
+import dash_auth
 import dash_bootstrap_components as dbc
 # Para poder hacer uso de la librería con componentes de bootstrap, es muy necesario
 # utilizar una hoja de estilos que contenga las clases bootstrap. En este caso se utiliza el tema por default
@@ -13,6 +14,17 @@ external_stylesheets = dbc.themes.PULSE #JOURNAL
 app = dash.Dash(__name__, external_stylesheets=[external_css,external_stylesheets],
     meta_tags=[{'name':'viewport',
     'content':'width=device-width, initial-scale = 1.0, maximum-scale=1.5, minimum-scale=1.0'}])
+
+# authenticación
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'sesna': 'dgria'
+}
+
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
 
 # la siguiente linea puede que se pueda quitar (se menciona en el archivo index.py que
 server = app.server
