dfd952f89ec4a26790439e91d36e242ed97bd5a8
SESNA-Inteligencia <jcmartinez@sesna.gob.mx>
2024-04-02 15:55:22 -0600

    Se actualizo el path

--- path.py
+++ path.py
@@ -1,6 +1,7 @@
 
+import os
 # ruta del visualizador
 #  Para correr el visualizador en forma local o en servidor se debe cambiar la dicrección 
 #  de la carpeta que contiene los archivos fuiente
-root = "C:/Users/jcmartinez/Desktop/ProgramasSociales"
+root = os.getcwd()
 #root = "/home/ubuntu/Desktop/Proyecto/ProgramasSociales"
